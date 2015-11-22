e2fa606e72e422177b8eeaf9844e170aa1eb6c5b
verodula <vijayshree.erodula@yale.edu>
2015-11-22 18:59:06 -0500

    Added logic to the function

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        # The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+        # set the value of the matrix
+        # get the value of the matrix
+        # set the value of the inverse matrix
+        # get the value of the inverse matrix
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) m <<- inverse
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
@@ -12,4 +28,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## funtion cacheSolve()
+
+        # get the inverse from cache by getinv function
+        i <- x$getinv()
+
+        # if inverse is calculated
+        if (!is.null(i)) {
+                # then get from the cache
+                message("getting cached inverse")
+                return(i)
+        }
+        # else, get the inverse
+        mat.data <- x$get()
+        i <- solve(mat.data, ...)
+
+        # set the inverse value in the cache
+        x$setinv(i)
+        return(i)
+
 }
