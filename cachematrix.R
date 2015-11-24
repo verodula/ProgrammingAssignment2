6a3d6a94a62bbad3852ecd51525675a850ab8375
verodula <vijayshree.erodula@yale.edu>
2015-11-23 19:27:04 -0500

    variable name change

--- cachematrix.R
+++ cachematrix.R
@@ -15,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 i <<- NULL
         }
         get <- function() x
-        setinv <- function(inverse) m <<- inverse
+        setinv <- function(inverse) i <<- inverse
         getinv <- function() i
         list(set = set, get = get,
              setinv = setinv,
